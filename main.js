643113c04d033b3118f27be106302d5ce364ad03
QuachDangDiep <145833428+QuachDangDiep@users.noreply.github.com>
2024-10-19 17:07:57 +0700

    .

--- main.js
+++ main.js
@@ -2734,6 +2734,186 @@ Thư viện: output luôn luôn là 1 promise
    // logger2('Test message...',constants.TYPE_ERROR);
 
 
+   // 13. Optional chaining (?.)
+
+   // const obj = {
+   //    name: 'Alice',
+   //    cat: {
+   //       name: 'Dinah',
+   //       cat2: {
+   //          name: 'Dinah 2',
+   //          cat3: {
+   //             name: 'Dinah 3'
+   //          }
+   //       }
+   //    }
+   // };
+
+   // console.log(obj.cat.cat2.cat3.name);
+
+
+   // if ( obj?.cat?.cat2?.cat3) {
+   //    console.log(obj.cat.cat2.cat3.name);
+   // }
+
+   // // console.log(obj.cat.name)
+
+   // const obj = {
+      // getName(value){
+      //    console.log(value);
+      // }
+   // }
+   // obj.getName?.(123);
+
+
+   // Promise example tiếp tục
+   // var users = [
+   //    {
+   //       id: 1,
+   //       name: 'Dang Diep',
+   //    },
+   //    {
+   //       id: 2,
+   //       name: 'Van Hoa'
+   //    },
+   //    {
+   //       id: 3,
+   //       name: 'Hung Dam'
+   //    },
+   // ];
+
+   // var comments = [
+   //    {
+   //       id: 1,
+   //       user_id: 1,
+   //       content: 'Tao cung chua lam'
+   //    },
+   //    {
+   //       id: 2,
+   //       user_id: 2,
+   //       content: 'Hoi xem ngoc lam chua'
+   //    },
+   //    {
+   //       id: 3,
+   //       user_id: 1,
+   //       content: 'OK de tao hoi'
+   //    },
+   // ]
+
+   // 1. Lấy comments
+   // 2. Từ comments Lấy ra user_id
+   // 3. Từ user_id lấy ra user tương ứng
+
+   // Fake API
+   // 1. Array
+   // 2. function, callback
+   // 3. Promise
+   // 4. DOM
+
+   // function getComments() {
+   //    return new Promise(function(resolve){
+   //       setTimeout(function(){
+   //          resolve(comments);
+   //       }, 1000);
+   //    });
+   // }
+
+   // function getUsersByIds(userIds){
+   //    return new Promise(function(resolve) {
+   //       var result = users.filter(function(user){
+   //          return userIds.includes(user.id);
+   //       });
+   //       setTimeout(function() {
+   //          resolve(result);
+   //       }, 1000);
+   //    });
+   // }
+
+   // getUsersByIds ([1, 2])
+   //    .then(function(users) {
+   //       console.log(users);
+   //    });
+
+   // getComments()
+   // .then(function(comments) {
+   //    // console.log(comments);
+   //    var userIds = comments.map(function(comment) {
+   //       return comment.user_id;
+   //    });
+
+   //    return getUsersByIds (userIds)
+   //       .then(function(users) {
+   //          // console.log(users);
+   //          return {
+   //             users: users,
+   //             comments: comments,
+   //          };
+   //       });
+   // })
+   // .then(function(data) {
+   //    // console.log(data);
+   //    var commentBlock = document.getElementById('Comment-block');
+
+   //    var html = '';
+   //    data.comments.forEach(function(comment) {
+   //       var user = data.users.find(function(user) {
+   //          return user.id === comment.user_id;
+   //       });
+   //       html += `<li>${user.name}: ${comment.content}</li>`;
+   //    });
+   //    commentBlock.innerHTML = html;
+   // });
+
+
+
+   //4. fetch
+
+   //1. front-end: Xây dựng giao diện
+   // người dùng 
+
+   //2. Back-end: Xây dựng logic xử lý
+   // + Cơ sở dữ liệu 
+
+   // API => Application programing interface: Cổng giao tiếp giữa các phần mềm
+
+   // Backend (OK) -> API (URL) -> Fetch -> JSON
+   // -> JSON.parse -> Javascript types
+   // -> Render ra dao diện với HTML
+
+   // var postAPI = 'https://jsonplaceholder.typicode.com/posts';
+
+
+   // streaming API
+   // fetch(postAPI)
+   //    .then(function(response){
+   //       return response.json();
+   //       // Json.parse: JSON -> JavaScript types
+   //    })
+   //    .then(function(posts) {
+   //       var htmls = posts.map(function(post){
+   //          return`<li>
+   //             <h2>${post.title}</h2>
+   //             <p>${post.body}</p>
+   //          </li>`;
+   //       });
+
+   //       var html = htmls.join('');
+   //       document.getElementById('post-block').innerHTML = html;
+   //    })
+   //    .catch(function(error){
+   //       alert('Có Lỗi!');
+   //    })
+
+
+      // face JSON server: API Server
+      var courseAPI = 'https://jsonplaceholder.typicode.com';
+      fetch(courseAPI)
+      .then(function(response) {
+         return response.json();
+      })
+      .then(function(courses) {
+         console.log(courses)
+      });
 
 
    
